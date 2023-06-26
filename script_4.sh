989b8c49ed00cbd892d915aa8205e3b293c2ed7a
Matsyshyn Anton <anton.matsyshyn@uklon.com.ua>
2023-06-26 09:33:13 +0300

    Add 4 task

---
+++ script_4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#PBS -l walltime=00:02:00
+#PBS -l nodes=1:ppn=1
+#PBS -N matsyshyn_integral
+#PBS -t 1-34
+
+cd $PBS_O_WORKDIR
+
+params=`sed -n "${PBS_ARRAYID}p" solv.inp`
+./my_integral $params > ~/$PBS_O_WORKDIR/solv.out
